b107b938c032ca4e2325d303988c529c9e89f041
Bansii00 <108782408+Bansii00@users.noreply.github.com>
2022-10-27 23:57:27 -0400

    project.

---
+++ src/main/java/org/ServletConfigDemo.java
@@ -0,0 +1,40 @@
+package org;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletConfigDemo
+ */
+public class ServletConfigDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		ServletConfig config=getServletConfig();
+		ServletContext ct=getServletContext();
+		String managerName=config.getInitParameter("managerName");
+		String managerEmail=ct.getInitParameter("managerEmail");
+		
+		out.println("<center> Manager Name         : "+managerName+"</center></br>");
+		out.println("<center> Manager Email Id     : "+managerEmail+"</center>");
+	
+	
+	}
+
+	
+
+}
